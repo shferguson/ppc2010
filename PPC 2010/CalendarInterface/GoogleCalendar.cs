3605265c618b35f4c76bb71ddf57e7110d60569b
Scott Ferguson <shferguson@gmail.com>
2015-02-22 22:00:54 -0500

    Fixed issues where the parts of multiple day events in the past would still display

--- PPC 2010/CalendarInterface/GoogleCalendar.cs
+++ PPC 2010/CalendarInterface/GoogleCalendar.cs
@@ -15,9 +15,9 @@ namespace PPC_2010.CalendarInterface
             _calendarService = calendarService;
         }
 
-        public List<CalendarItem> GetCalendarItems(string calendarId, DateTime startDate, DateTime endDate)
+        public List<CalendarItem> GetCalendarItems(string calendarId, DateTime listStartDate, DateTime listEndDate)
         {
-            List<CalendarItem> items = HttpContext.Current.Cache[BuildCacheKey(startDate, endDate)] as List<CalendarItem>;
+            List<CalendarItem> items = HttpContext.Current.Cache[BuildCacheKey(listStartDate, listEndDate)] as List<CalendarItem>;
             if (items == null)
             {
                 try
@@ -25,8 +25,8 @@ namespace PPC_2010.CalendarInterface
                     items = new List<CalendarItem>();
 
                     var listRequest = _calendarService.Service.Events.List(calendarId);
-                    listRequest.TimeMin = startDate;
-                    listRequest.TimeMax = endDate;
+                    listRequest.TimeMin = listStartDate;
+                    listRequest.TimeMax = listEndDate;
                     listRequest.SingleEvents = true;    // Show recurring events as invividual events
                     listRequest.Fields = "items(end,start,summary)";
                     
@@ -37,7 +37,7 @@ namespace PPC_2010.CalendarInterface
                         bool isAlldayEvent = eventItem.Start.DateTime == null;
                         if (isAlldayEvent)
                         {
-                            AddItemsForAllDateEvent(endDate, items, eventItem);
+                            AddItemsForAllDateEvent(listStartDate, listEndDate, items, eventItem);
                         }
                         else {
                             items.Add(new CalendarItem
@@ -50,15 +50,15 @@ namespace PPC_2010.CalendarInterface
                         }
                     }
 
-                    HttpContext.Current.Cache.Add(BuildCacheKey(startDate, endDate), items, null, DateTime.Now.AddSeconds(120), Cache.NoSlidingExpiration, CacheItemPriority.Normal, null);
-                    HttpContext.Current.Application.Contents[BuildCacheKey(startDate, endDate)] = items;
+                    HttpContext.Current.Cache.Add(BuildCacheKey(listStartDate, listEndDate), items, null, DateTime.Now.AddSeconds(120), Cache.NoSlidingExpiration, CacheItemPriority.Normal, null);
+                    HttpContext.Current.Application.Contents[BuildCacheKey(listStartDate, listEndDate)] = items;
                 }
                 catch (Exception ex)
                 {
                     // The Goolge calendar API seems to fail at times, swallow the exception and try to use a long lived cache item
                     Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error(ex, HttpContext.Current));
 
-                    items = HttpContext.Current.Application[BuildCacheKey(startDate, endDate)] as List<CalendarItem>;
+                    items = HttpContext.Current.Application[BuildCacheKey(listStartDate, listEndDate)] as List<CalendarItem>;
                 }
             }
 
@@ -68,15 +68,16 @@ namespace PPC_2010.CalendarInterface
             return items;
         }
 
-        private static void AddItemsForAllDateEvent(DateTime endDate, List<CalendarItem> items, Event eventItem)
+        private static void AddItemsForAllDateEvent(DateTime listStartDate, DateTime listEndDate, List<CalendarItem> items, Event eventItem)
         {
-            // Google only returns one event for a multi-day all day event, we need to add an individual CalendarItem
-            // for each day
-
+            // Google only returns one event for a multi-day all day event, we need to add an individual CalendarItem for each day
             DateTime eventStartDate, eventEndDate;
             if (DateTime.TryParse(eventItem.Start.Date, out eventStartDate) && DateTime.TryParse(eventItem.End.Date, out eventEndDate))
             {
-                while (eventStartDate < eventEndDate && eventStartDate < endDate)
+                if (eventStartDate.Date < listStartDate.Date)
+                    eventStartDate = listStartDate;
+
+                while (eventStartDate < eventEndDate && eventStartDate < listEndDate)
                 {
                     items.Add(new CalendarItem
                     {
