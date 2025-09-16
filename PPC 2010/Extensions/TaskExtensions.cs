042efa124e75780588844c55528c1915e64b9786
Scott Ferguson <shferguson@gmail.com>
2025-09-16 13:58:51 -0400

    Project cleanup

---
+++ PPC 2010/Extensions/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PPC_2010.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static void RunInBackground(Func<Task> action)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    await action();
+                }
+                catch (Exception ex)
+                {
+                    Elmah.ErrorLog.GetDefault(null).Log(new Elmah.Error(ex));
+                }
+            });
+        }
+    }
+}
